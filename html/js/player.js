3f86f1911ecc7078fa68f01a8ba3190671a993af
Shahor <shahor@shahor.fr>
2011-04-15 20:10:08 +0200

    Collisions
    	* Can't enter indestructible rock again : Done :)

--- html/js/player.js
+++ html/js/player.js
@@ -15,7 +15,7 @@ Player.prototype.draw = function (ctx) {
 Player.prototype.move = function(direction) {
 	switch (direction) {
 		case 'right': 
-			if (this.x + this.img.width >= Game.board.canvas.width || this.wouldCollideWithIndestructibleWall()) {
+			if (this.x + this.img.width >= Game.board.canvas.width || this.wouldCollideWithIndestructibleWall(direction)) {
 				Debug.warn("Can't move right anymore");
 				break;
 			}
@@ -24,14 +24,14 @@ Player.prototype.move = function(direction) {
 
 			break;
 		case 'left':
-			if (this.x <= 0 || this.wouldCollideWithIndestructibleWall()) {
+			if (this.x <= 0 || this.wouldCollideWithIndestructibleWall(direction)) {
 				Debug.warn("Can't move left anymore");
 				break;
 			}
 			this.x -= this.speed;
 			break;
 		case 'up':
-			if (this.y <= 0) {
+			if (this.y <= 0 || this.wouldCollideWithIndestructibleWall(direction)) {
 				Debug.warn("Can't move upwards anymore");
 				break;
 			}
@@ -39,7 +39,7 @@ Player.prototype.move = function(direction) {
 			this.y -= this.speed;
 			break;
 		case 'down':
-			if (this.y + this.img.height >= Game.board.canvas.height)
+			if (this.y + this.img.height >= Game.board.canvas.height || this.wouldCollideWithIndestructibleWall(direction))
 			{
 				Debug.warn("Can't move downwards anymore");
 				break;
@@ -52,20 +52,47 @@ Player.prototype.move = function(direction) {
 	}
 }
 
-Player.prototype.wouldCollideWithIndestructibleWall = function () {
+Player.prototype.wouldCollideWithIndestructibleWall = function (direction) {
 	var rightBorder = this.x + this.img.width;
 	var bottomBorder = this.y + this.img.height;
 	
 	var imageTopIsInLineNumber = this.y / this.img.height;
+	var imageLeftIsInColumnNumber = this.x / this.img.width;
 	var imageBottomIsInLineNumber = bottomBorder / this.img.height;
+	var imageRightIsInColumnNumber = rightBorder / this.img.width;
 	
-	var collisionRight = (!check.isInt(imageBottomIsInLineNumber) && !check.isInt(imageTopIsInLineNumber) && check.isInt(rightBorder / 40) 
-	&& (parseInt(imageTopIsInLineNumber) % 2 === 0) || (parseInt(imageBottomIsInLineNumber) % 2 === 0));
-	
-	var collisionLeft = (!check.isInt(imageBottomIsInLineNumber) && !check.isInt(imageTopIsInLineNumber) && check.isInt(this.x / 40) 
-	&& (parseInt(imageTopIsInLineNumber) % 2 === 0) || (parseInt(imageBottomIsInLineNumber) % 2 === 0));
-	
-	if (collisionRight) return true;
-	if (collisionLeft) return true;
+	switch (direction)
+	{
+		case 'right':
+			if ((!check.isInt(imageBottomIsInLineNumber) && !check.isInt(imageTopIsInLineNumber) && check.isInt(rightBorder / 40) 
+				&& (parseInt(imageTopIsInLineNumber) % 2 === 0) || (parseInt(imageBottomIsInLineNumber) % 2 === 0)))
+			{
+				return true;
+			}
+			break;
+		case 'left':
+			if ((!check.isInt(imageBottomIsInLineNumber) && !check.isInt(imageTopIsInLineNumber) && check.isInt(this.x / 40) 
+				&& (parseInt(imageTopIsInLineNumber) % 2 === 0) || (parseInt(imageBottomIsInLineNumber) % 2 === 0))) 
+			{
+				return true;
+			}
+			break;
+		case 'up':
+			if ((!check.isInt(imageLeftIsInColumnNumber) && !check.isInt(imageRightIsInColumnNumber) && check.isInt(this.y / 40) 
+				&& (parseInt(imageRightIsInColumnNumber) % 2 === 1) || (parseInt(imageLeftIsInColumnNumber) % 2 === 1)))
+			{
+				return true;
+			}
+			break;
+		case 'down':
+				if ((!check.isInt(imageLeftIsInColumnNumber) && !check.isInt(imageRightIsInColumnNumber) && check.isInt(bottomBorder / 40) 
+					&& (parseInt(imageRightIsInColumnNumber) % 2 === 1) || (parseInt(imageLeftIsInColumnNumber) % 2 === 1)))
+				{
+					return true;
+				}
+				break;
 		
+		default: 
+			Debug.error('Not implemented yet');
+	}
 }
